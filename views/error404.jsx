f2aaaedb76b7caebbe93dc8afa33f50725be9175
Angad <angadbhatti18@gmail.com>
2022-03-15 16:05:00 -0700

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                <img className="imgClass" src="/images/spider-sense.png" alt="Spider-Man spider-sense" />
+                <div>
+                  Photo from <a href="https://comicvine.gamespot.com/a/uploads/scale_small/8/84205/2309538-spider_sense_by_spideray_d4mzxm8.png">Comic Vine</a>
+                </div>
+              </div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img className="imgClass" src="/images/iced-coffee-in-glass.jpg" alt="Iced Coffee in Glass on Table from unsplash" />
+                <div>
+                  Photo by <a href="https://unsplash.com/@demidearest?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Demi DeHerrera</a> on <a href="https://unsplash.com/@demidearest?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+              </div>
               <a href="/places">
                 <button className="btn-primary">Places Page</button>
               </a>
